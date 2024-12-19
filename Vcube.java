76d1ce419572cae7fdc18adaadf90ca7bf91f34e
Vinaykumarpolepalli <vinaykumarpolepalli@gmail.com>
2024-12-19 19:18:50 +0530

    Add files via upload

---
+++ Vcube.java
@@ -0,0 +1,16 @@
+package com.langfund;
+
+import java.math.BigDecimal;
+
+public class Vcube {
+
+	public static void main(String[] args) {
+		BigDecimal d1 = new  BigDecimal ("4567.876543");
+		BigDecimal d2 = new BigDecimal  (34567);
+		
+		System.out.println(d1);
+		System.out.println(d2);
+
+	}
+
+}
