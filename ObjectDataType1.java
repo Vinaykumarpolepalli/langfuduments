f94f9e1f4c6dc160ac233edfd231338a4b023b2f
Vinaykumarpolepalli <vinaykumarpolepalli@gmail.com>
2024-12-19 19:17:31 +0530

    Add files via upload

---
+++ ObjectDataType1.java
@@ -0,0 +1,18 @@
+package com.langfund;
+
+public class ObjectDataType1 {
+	String  s = "vinay kumar java";
+	String s1 = new String();
+	String s2;
+ 
+	public static void main(String[] args) {
+		ObjectDataType1 o = new ObjectDataType1();
+		System.out.println( "string :"+o.s);
+		System.out.println(o.s1);
+		System.out.println("hello vinay");
+		System.out.println(o.s2);
+
+     
+	}
+
+}
