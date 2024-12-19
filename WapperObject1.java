5e84ea84e4059e9750fef69c5017210ccd547146
Vinaykumarpolepalli <vinaykumarpolepalli@gmail.com>
2024-12-19 19:19:08 +0530

    Add files via upload

---
+++ WapperObject1.java
@@ -0,0 +1,21 @@
+package com.langfund;
+
+public class WapperObject1 {
+	byte b = (byte)234;
+    short s = (short ) 123457689;
+    float f = (float ) 456789097;
+    //Bigintger = new Bigintger( 23456);
+    //Address  address = new Address();
+    
+    
+    
+    
+    
+    
+	 public static void main (String []args) {
+		 WapperObject1 w = new WapperObject1();
+		 System.out.println( "wapper objet data type convert into the  byte to intger:"+w.b );
+		 System.out.println( "wapper object data type convert into the short to intger:"+w.s);
+		 System.out.println(w.f);
+	 }
+}
