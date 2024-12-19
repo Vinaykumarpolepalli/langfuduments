c42a8b778f689a56ea3ffa7df01ff68976c90f19
Vinaykumarpolepalli <vinaykumarpolepalli@gmail.com>
2024-12-19 19:07:52 +0530

    Add files via upload

---
+++ Literals1.java
@@ -0,0 +1,29 @@
+package com.langfund;
+
+public class Literals1 {
+	
+	String name = "vinay";
+	int i = 12345;
+	int i1 = 067;
+	int i2 = 0x5678;
+	float f = 0xdad;
+	
+
+	public static void main(String[] args) {
+	 Literals1 l = new Literals1();
+	System.out.println(l.name);
+	System.out.println(l.i);
+	System.out.println(l.i1);
+	System.out.println(l.i2);
+	System.out.println(l.f);
+	}
+
+}
+
+
+/*integer 
+ * float
+ * char
+ * boolean
+ * string
+ * null*/
