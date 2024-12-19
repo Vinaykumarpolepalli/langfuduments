5efbb03b385eaa108443b845f52b4a27ce431d54
Vinaykumarpolepalli <vinaykumarpolepalli@gmail.com>
2024-12-19 19:13:39 +0530

    Add files via upload

---
+++ Literals2.java
@@ -0,0 +1,178 @@
+package com.langfund;
+
+public class Literals2 {
+	
+	
+//	literals (int i) {
+//		System.out.println(i);
+		
+
+
+	int i0;
+	public static void main(String[] args) {
+		main1();
+	 
+//		int i ;
+		int i1 = 1234;
+		int i2 = 34567;
+		int i3 = 0123;
+		int i4 = 0x123;
+		int i5 = 0xdad;
+		int i6 = 0xbee;
+		int i7 = 05234;
+		int i8 = 0x75ab;
+		int i9 = 0xabcdf;
+		
+		
+		System.out.println(i1);
+		System.out.println(i2);
+		System.out.println(i3);
+		System.out.println(i4);
+		System.out.println(i5);
+		System.out.println(i6);
+		System.out.println(i7);
+		System.out.println(i8);
+		System.out.println(i9);
+
+	}
+	public static void main1 () {
+		
+	   float f = 123;
+	   float f1 = 0123f;
+	   float f2 = 0123.5f;
+//	   float f3 = 01230.5df;
+	   float f4 = 1230.0f;
+	   float f5 = 0x123f;
+	   float f6 = 0xdadf;
+	   float f7= 0xaf;
+	   float f8 = 0x768;
+	
+	System.out.println(f);
+	System.out.println(f1);
+	System.out.println(f2);
+//	System.out.println(f3);
+	System.out.println(f4);
+	System.out.println(f5);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	System.out.println(f6);
+	System.out.println(f7);
+	System.out.println(f8);
+
+	
+	
+	}
+}
+
