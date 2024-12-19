f9b317ad707f5a2461a79db5e2a2627875c43e65
Vinaykumarpolepalli <vinaykumarpolepalli@gmail.com>
2024-12-19 19:18:05 +0530

    Add files via upload

---
+++ Testq2.java
@@ -0,0 +1,19 @@
+package com.langfund;
+
+public class Testq2 {
+	static int x =90;
+
+	public static void main(String[] args) {
+//		Testq2 t = new Testq2();
+		System.out.println(x);
+        System.out.println(x);
+	
+		
+
+	}
+
+	/*static {
+	 
+	    int  = 20;
+	}*/
+}
