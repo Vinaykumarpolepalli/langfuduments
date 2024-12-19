af0b5054f64a63ca6325e5e17cf497b1efd1534a
Vinaykumarpolepalli <vinaykumarpolepalli@gmail.com>
2024-12-19 19:03:12 +0530

    Add files via upload

---
+++ BigInteger1.java
@@ -0,0 +1,15 @@
+package com.langfund;
+
+import java.math.BigInteger;
+
+public class BigInteger1 {
+	
+//	BigInteger1 b = new BigInteger1("12345");
+	
+	public static void main (String []args) {
+	BigInteger b1 = new BigInteger("23456789");
+	System.out.println(b1);
+
+	}
+
+}
