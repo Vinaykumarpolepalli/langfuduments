ed4fe42a06426ff6ddc6361b6713e766440fb319
Vinaykumarpolepalli <vinaykumarpolepalli@gmail.com>
2024-12-19 19:17:48 +0530

    Add files via upload

---
+++ Testq1.java
@@ -0,0 +1,27 @@
+package com.langfund;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+public class Testq1 {
+	BigInteger b = new  BigInteger("234576");
+	BigDecimal d1 = new  BigDecimal("4567.56789");
+	BigDecimal d2 = new  	BigDecimal(345678);
+
+	
+	
+	public static void main(String[] args) {
+	Testq1 t = new Testq1();
+
+		 System.out.println(t.d1);
+		 System.out.println(t.d2);
+		
+		/*float f = 6.789f;
+		System.out.println(String.format("%.3f",f));*/
+		
+		
+		System.out.println(t.b);
+	} 
+	
+
+}
