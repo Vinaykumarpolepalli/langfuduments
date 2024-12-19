f1817ca8986c7ed7cfb261ef204b91eb4000415c
Vinaykumarpolepalli <vinaykumarpolepalli@gmail.com>
2024-12-19 19:14:28 +0530

    Add files via upload

---
+++ Literals3.java
@@ -0,0 +1,100 @@
+package com.langfund;
+
+import java.math.BigInteger;
+import java.math.BigDecimal;
+
+public class Literals3 {
+	
+	BigInteger b1 ;
+	BigDecimal b=new BigDecimal("47");
+	BigDecimal d1;
+	public static void main(String[] args) {
+		
+//		BigDecimal  d2 = 234;
+		
+		Literals3 l = new Literals3();
+		System.out.println(l.b); 
+		System.out.println(l.b1);
+		System.out.println(l.d1);
+		
+		//l.main1();
+		
+		/*float f = 123;
+		float f1 = 0123f;
+		float f2 = 0123.5f;
+//		//float f3 = 01230.5d;
+		float f4 = 1230.0f;
+		float f5 = 0x123;
+		float f6 = 0xdad;
+		float f7= 0xaf;
+		//float f8 = 0x123.5f;
+		float f9  =  1230.0f;
+		
+		System.out.println(f9);
+		System.out.println(f1);
+		System.out.println(f2);
+		System.out.println(f4);
+		System.out.println(f5);
+		System.out.println(f6);
+		System.out.println(f7);
+		//System.out.println(f8);  */
+		
+		
+
+	}
+	
+	
+	//Double ***************************
+	
+	 /*public void main1() {
+		double d1 = 123;
+		double d2 = 0123;
+		double d3 = 0123d;
+		double d4 = 0123.0d;
+		double d5 = 0123.5d;
+		double d6 = 0.123d;
+		double d7 = 0x123d;
+		
+		
+		System.out.println(d1);
+	    System.out.println(d2);
+	    System.out.println(d3);
+		System.out.println(d4);
+		System.out.println(d5);
+		System.out.println(d6);
+		System.out.println(d7); 
+		
+		
+		
+	}*/
+
+     /*public void main1() {
+    	 char c1 = 'v';
+    	 char c2 = '9';
+    	 char c3 = '\u0063';
+    	 char c4 = '\u003D';
+    	 char c5 = 98;
+    	 
+    	System.out.println(c1); 
+    	System.out.println(c2); 
+    	System.out.println(c3); 
+    	System.out.println(c4); 
+    	System.out.println(c5); 
+     }*/
+	/*public void main1 () {
+		boolean  b1 =  true;
+		boolean b2 = false;
+		System.out.println(b1); 
+		System.out.println(b2); 
+	}*/
+	
+	/*public void main1 () {
+		String s1 = "vinay";
+		String s2 = "234";
+		System.out.println(s1);
+		System.out.println(s2); 
+	} */
+	
+
+}
+
