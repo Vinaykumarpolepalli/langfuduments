44b166f3bbc3782071ec6bdcf72e10105eaf0f8c
Vinaykumarpolepalli <vinaykumarpolepalli@gmail.com>
2024-12-19 19:17:10 +0530

    Add files via upload

---
+++ Literals4.java
@@ -0,0 +1,49 @@
+package com.langfund;
+
+public class Literals4 {
+
+	public static void main(String[] args) {
+//		int a =  123;
+//		int a1 = 1230;
+//		int a2 = 0123;
+//		int  a3 = 0x123;
+//		
+//		
+//
+//		char c1 = 'v';
+//		char c2 = 99;
+//		char c3 ='\u0034';
+//		char c4 = 65;
+//		
+		
+//		System.out.println(a);
+//		System.out.println(a1);
+//		System.out.println(a2);
+//		System.out.println(a3);
+//		
+////		
+////		System.out.println(f1);
+//		System.out.println(f2);
+//		
+////		System.out.println(d1);
+//		System.out.println(d2);
+//		System.out.println(d3);
+//		System.out.println(d4);
+//		System.out.println(d5);
+//		System.out.println(d6);
+//		System.out.println(d7);
+//		System.out.println(d8);
+//		System.out.println(d9);
+//		System.out.println(d10);
+//		System.out.println(d11);
+//	
+//		System.out.println(c1);
+//		System.out.println(c2);
+//		System.out.println(c3);
+//		System.out.println(c4);
+
+		
+		
+	}
+
+}
